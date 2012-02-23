9dfc448c441522756b96aa36a0345325f7bde090
mcmullen_brown <none@none>
2012-02-23 22:27:24 +0000

    Got a start on the particle class
    --HG--
    extra : convert_revision : svn%3Acdefcc1b-bc0c-407a-80b0-b0ae7b0029a2%4033

--- ParticleStorm/particle.cpp
+++ ParticleStorm/particle.cpp
@@ -1,5 +1,31 @@
 #include "particle.h"
+#include "qmath.h"
+
 
 Particle::Particle():GameObject(){
 
+
+
+}
+
+Particle::update(const double dt) {
+    //update positions
+    x += xVel*dt;
+    y += yVel*dt;
+
+    //update tail length?
+
+
+}
+
+Particle::init() {
+    mass = 1;
+    xVel = 0;
+    yVel = 0;
+
+    updateColour();
+}
+
+Particle::updateColour() {
+    //set a new colour based on the new speed
 }
