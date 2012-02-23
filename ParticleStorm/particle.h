53069deedb9a42330a8fe421a03012be163320cc
mcmullen_brown <none@none>
2012-02-23 22:28:04 +0000

    particle header file
    --HG--
    extra : convert_revision : svn%3Acdefcc1b-bc0c-407a-80b0-b0ae7b0029a2%4034

--- ParticleStorm/particle.h
+++ ParticleStorm/particle.h
@@ -7,6 +7,20 @@ class Particle : public GameObject
 {
 public:
     Particle();
+    void update();
+    void applyForce(double x, double y); //don't need x, y, and mag (mag can be derived from x,y)
+
+
+
+private:
+    //internally change the particle's colour to match its speed
+    void updateColour();
+    void init();
+
+    double mass;
+    double x,y;
+    double xVel,yVel;
+
 };
 
 #endif // PARTICLE_H
