520784f7e6118f5f4fab6fb780f391753a64cef8
kester_brown <none@none>
2012-03-25 22:13:21 +0000

    levelmanager adds all types of enemies based on level
    --HG--
    extra : convert_revision : svn%3Acdefcc1b-bc0c-407a-80b0-b0ae7b0029a2%40145

--- ParticleStorm/levelmanager.cpp
+++ ParticleStorm/levelmanager.cpp
@@ -3,16 +3,21 @@
 LevelManager::LevelManager() {currLvl=1;}
 
 void LevelManager::startLevel() {
-
-    //ObjectManager::getInstance()->spawnEnemy(ObjectManager::GRUNT, 500, 100, ObjectManager::getInstance()->getPlayer()->getX(), ObjectManager::getInstance()->getPlayer()->getY());
-    //ObjectManager::getInstance()->spawnEnemy(ObjectManager::HEALER, 500, 100, ObjectManager::getInstance()->getPlayer()->getX(), ObjectManager::getInstance()->getPlayer()->getY());
-    //ObjectManager::getInstance()->spawnEnemy(ObjectManager::TANK, 500, 100, ObjectManager::getInstance()->getPlayer()->getX(), ObjectManager::getInstance()->getPlayer()->getY());
-    //ObjectManager::getInstance()->spawnEnemy(ObjectManager::SPRINTER, 500, 100, ObjectManager::getInstance()->getPlayer()->getX(), ObjectManager::getInstance()->getPlayer()->getY());
-
-    for (int i=0; i<10;i++) {
-        int tempx = qrand()%GameEngine::MAX_X;
-        int tempy = qrand()%GameEngine::MAX_Y;
-        ObjectManager::getInstance()->spawnEnemy(ObjectManager::SHOOTER, tempx, tempy, ObjectManager::getInstance()->getPlayer()->getX(), ObjectManager::getInstance()->getPlayer()->getY());
+    int temp = currLvl*currLvl*100;
+    for (int i=0; i<(temp*5);i++) {
+        ObjectManager::getInstance()->spawnEnemy(ObjectManager::SHOOTER, qrand()%GameEngine::MAX_X, qrand()%GameEngine::MAX_Y, ObjectManager::getInstance()->getPlayer()->getX(), ObjectManager::getInstance()->getPlayer()->getY());
+    }
+    for (int i=0; i<(temp*10);i++) {
+        ObjectManager::getInstance()->spawnEnemy(ObjectManager::GRUNT, qrand()%GameEngine::MAX_X, qrand()%GameEngine::MAX_Y, ObjectManager::getInstance()->getPlayer()->getX(), ObjectManager::getInstance()->getPlayer()->getY());
+    }
+    for (int i=0; i<(temp*10);i++) {
+        ObjectManager::getInstance()->spawnEnemy(ObjectManager::HEALER, qrand()%GameEngine::MAX_X, qrand()%GameEngine::MAX_Y, ObjectManager::getInstance()->getPlayer()->getX(), ObjectManager::getInstance()->getPlayer()->getY());
+    }
+    for (int i=0; i<(temp*10);i++) {
+        ObjectManager::getInstance()->spawnEnemy(ObjectManager::TANK, qrand()%GameEngine::MAX_X, qrand()%GameEngine::MAX_Y, ObjectManager::getInstance()->getPlayer()->getX(), ObjectManager::getInstance()->getPlayer()->getY());
+    }
+    for (int i=0; i<(temp*5);i++) {
+        ObjectManager::getInstance()->spawnEnemy(ObjectManager::SPRINTER, qrand()%GameEngine::MAX_X, qrand()%GameEngine::MAX_Y, ObjectManager::getInstance()->getPlayer()->getX(), ObjectManager::getInstance()->getPlayer()->getY());
     }
 }
 
